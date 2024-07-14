c480ea4d93544cd6ce066f5239d9ebf7cd67c763
[Nicholas Sisk] <nsisk1@student.dallascollege.edu>
2024-07-14 17:11:22 -0500

    added php to take care of redirect and the email

---
+++ .history/process_form_20240714170607.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "nsisk1@student.dallascollege.edu"; // Replace with your email address
+    $from = $_POST['email'];
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $headers = "From:" . $from;
+
+    if (mail($to, $subject, $message, $headers)) {
+        // Redirect to thank you page
+        header("Location: thank_you.php");
+        exit();
+    } else {
+        echo "Mail sending failed.";
+    }
+}
+?>
---
+++ .history/process_form_20240714170644.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "nsisk1@student.dallascollege.edu";
+    $from = $_POST['email'];
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $subject = "Form submission";
+    $message = $firstname . " " . $lastname . " wrote the following:" . "\n\n" . $_POST['message'];
+    $headers = "From:" . $from;
+
+    if (mail($to, $subject, $message, $headers)) {
+        // Redirect to thank you page
+        header("Location: thank_you.php");
+        exit();
+    } else {
+        echo "Mail sending failed.";
+    }
+}
+?>
